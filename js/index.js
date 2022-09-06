5dc1368b3cd771c53d9bf39eb25ccbe2974bb659
Danil <dantereshenko69@gmail.com>
2022-09-06 23:42:23 +0600

    fix: delete unnecesery import

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ import {greetings,setName} from './modules/greetings.js';
 import {getImg,Slider} from './modules/randomImage.js';
 import {getWeather} from './modules/weather.js';
 import {randomQuotes} from './modules/randomQuates.js';
-import {PlayOrPauseAudio} from './modules/audioPlayer.js';
 
 setName();
 setInterval(getTime, 0);
